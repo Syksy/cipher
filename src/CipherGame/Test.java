9ed4a3d14d566965b6492c5d63c02ce121743fe0
Teemu Daniel Laajala <teemu.daniel.laajala@gmail.com>
2018-07-09 23:15:02 +0300

    r4

--- src/CipherGame/Test.java
+++ src/CipherGame/Test.java
@@ -5,12 +5,28 @@
  */
 package CipherGame;
 
+// Java MySQL packages
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author teelaa
  */
 public class Test {
-    public static void main(String[] args){
-        
-    }
+    public static void main(String[] args) throws ClassNotFoundException, SQLException{
+        try{
+            // Setting up an example MySQL DB
+            Connection conn = null;
+            Class.forName("com.mysql.jdbc.Driver");
+            DriverManager.getConnection("jdbc:mysql://localhost/test","root", "");
+            System.out.print("Successfully connected to the dummy MySQL database");
+            conn.close();
+        }
+        catch(Exception e){
+            System.out.print("Error connecting to the MySQL DB:" + e.toString());
+        }
+    }    
 }
